8a6699e5611ea7c7d663ab731c4021e35e4512b8
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2025-02-27 18:28:30 +0100

    RobotomizationForm

---
+++ Cpp05/ex02/RobotomyRequestForm.hpp
@@ -0,0 +1,25 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+#include "AForm.hpp"
+#include <cstdlib>
+
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string	target;
+
+	public:
+		RobotomyRequestForm();
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm &src);
+		~RobotomyRequestForm();
+
+		RobotomyRequestForm	&operator=(RobotomyRequestForm const & rhs);
+
+		const	std::string &getTarget() const;
+
+		void	execute(Bureaucrat const & executor) const;
+};
+
+#endif
--- Cpp05/ex02/main.cpp
+++ Cpp05/ex02/main.cpp
@@ -12,17 +12,20 @@
 
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
-static void	testShrubberyCreationForm();
+static void testShrubberyCreationForm();
+static void testRobotomyRequestForm();
 
 int main(void)
 {
 	testShrubberyCreationForm();
+	testRobotomyRequestForm();
 
 	return 0;
 }
 
-static void	testShrubberyCreationForm()
+static void testShrubberyCreationForm()
 {
 	Bureaucrat bob("Bob", 1);
 	Bureaucrat alice("Alice", 150);
@@ -59,7 +62,50 @@ static void	testShrubberyCreationForm()
 	try {
 		std::cout << "Signing form2 with Bob and executing:" << std::endl;
 		form2.beSigned(bob);
+		bob.executeForm(form2);
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+}
+
+static void testRobotomyRequestForm()
+{
+	Bureaucrat bob("Bob", 1);
+	Bureaucrat alice("Alice", 150);
+
+	RobotomyRequestForm form1("Target1");
+	RobotomyRequestForm form2("Target2");
+
+	std::cout << "\n||||||ROBOTOMY REQUEST FORM TESTS||||||\n" << std::endl;
+
+	try {
+		std::cout << "Trying to execute form1 without signing:" << std::endl;
+		form1.execute(bob);
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "Signing form1 with Bob:" << std::endl;
+		form1.beSigned(bob);
+		std::cout << "Executing form1 with Bob:" << std::endl;
 		bob.executeForm(form1);
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "Trying to execute form2 with Alice (insufficient grade):" << std::endl;
+		form2.beSigned(alice);
+		form2.execute(alice);
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	try {
+		std::cout << "Signing form2 with Bob and executing:" << std::endl;
+		form2.beSigned(bob);
+		bob.executeForm(form2);
 	} catch (std::exception &e) {
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
