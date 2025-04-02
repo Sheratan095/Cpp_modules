c374bed859a5e122b5cfa6573eb6d5c90f9fe1d7
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-04-03 00:54:21 +0200

    Completed Cpp06

--- Cpp06/ex02/main.cpp
+++ Cpp06/ex02/main.cpp
@@ -1,8 +1,95 @@
+#include <cstdlib>
+#include <ctime>
+
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
+Base	*generate( void )
+{
+	// Generate a random number between 0 and 2
+	int	rndNumber = rand() % 3;
+
+	switch (rndNumber)
+	{
+		case 0:
+			return (new A());
+			break;
+	
+		case 1:
+			return (new B());
+			break;
+
+		case 2:
+			return (new C());
+			break;
+	}
+
+	return (NULL);
+}
+
+// dynamic_cast, if used with pointers, will return a null pointer if the cast fails
+// 	and will return a valid pointer if the cast is successful
+void	identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "\"A\"" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "\"B\"" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "\"C\"" << std::endl;
+}
+
+// (void)a/b/c is used to avoid unused variable warnings
+// dynamic_cast, if used with references, will throw an exception if the cast fails
+// 	and will return a valid reference if the cast is successful
+void	identify(Base& p)
+{
+	try
+	{
+		A	&a = dynamic_cast<A&>(p);
+		std::cout << "\"A\"" << std::endl;
+		(void)a;
+	}
+	catch (std::exception &ex){}
+
+	try
+	{
+		B	&b = dynamic_cast<B&>(p);
+		std::cout << "\"B\"" << std::endl;
+		(void)b;
+	}
+	catch (std::exception &ex){}
+
+	try
+	{
+		C	&c = dynamic_cast<C&>(p);
+		std::cout << "\"C\"" << std::endl;
+		(void)c;
+	}
+	catch (std::exception &ex){}
+}
+
 int main()
 {
+	// Used to get a different random number each time the program runs
+	std::srand(std::time(0));
 
+	Base *prova1 = generate();
+	Base *prova2 = generate();
+	Base *prova3 = generate();
+
+	identify(prova1);
+	identify(prova2);
+	identify(prova3);
+
+	std::cout << std::endl << "";
+
+	identify(*prova1);
+	identify(*prova2);
+	identify(*prova3);
+
+	delete (prova1);
+	delete (prova2);
+	delete (prova3);
 }
