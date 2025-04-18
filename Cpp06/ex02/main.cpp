8b910d3469061ff82e1eaa47766c16d8c0afc1ee
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-04-19 00:52:38 +0200

    unknown case in conversions

--- Cpp06/ex02/main.cpp
+++ Cpp06/ex02/main.cpp
@@ -51,6 +51,8 @@ void	identify(Base* p)
 		std::cout << "\"B\"" << std::endl;
 	else if (dynamic_cast<C*>(p))
 		std::cout << "\"C\"" << std::endl;
+	else
+		std::cout << "UNKNOWN" << std::endl;
 }
 
 // (void)a/b/c is used to avoid unused variable warnings
@@ -63,6 +65,7 @@ void	identify(Base& p)
 		A	&a = dynamic_cast<A&>(p);
 		std::cout << "\"A\"" << std::endl;
 		(void)a;
+		return ;
 	}
 	catch (std::exception &ex){}
 
@@ -71,6 +74,7 @@ void	identify(Base& p)
 		B	&b = dynamic_cast<B&>(p);
 		std::cout << "\"B\"" << std::endl;
 		(void)b;
+		return ;
 	}
 	catch (std::exception &ex){}
 
@@ -79,8 +83,11 @@ void	identify(Base& p)
 		C	&c = dynamic_cast<C&>(p);
 		std::cout << "\"C\"" << std::endl;
 		(void)c;
+		return ;
 	}
 	catch (std::exception &ex){}
+
+	std::cout << "UNKNOWN" << std::endl;
 }
 
 int main()
