5042e268b8379bcc82e91a23af96003cdc2bcf43
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-04-19 12:34:59 +0200

    added provided main

--- Cpp07/ex02/main.cpp
+++ Cpp07/ex02/main.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
-#include "Array.hpp"
+#include <Array.hpp>
 
-int main(int, char**)
+#define MAX_VAL 750
+
+int	main(int, char**)
 {
-	Array<int> numbe;
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-	Array<float> numbers(10);
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return (1);
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-	Array<float> test(numbers);
-
-	for (float i = 0; i < 10; i++)
-		numbers[i] = i / 2;
-
-	for (float i = 0; i < 10; i++)
-		std::cout << numbers[i] << std::endl;
-
-	std::cout << numbers.size() << std::endl;
-
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] (mirror);//
 	return (0);
 }
