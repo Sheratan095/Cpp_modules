51010b4dd91604674f7113f2be46b633c33459c9
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-05-07 11:43:22 +0200

    some makefile fixes

--- Cpp08/ex02/main.cpp
+++ Cpp08/ex02/main.cpp
@@ -14,14 +14,14 @@ int main()
 	mstack.push(737);
 	//[...]
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	// MutantStack<int>::iterator it = mstack.begin();
+	// MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
 	return 0;
