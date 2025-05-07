f93b1976e020d3672085b997314c48d9a5415500
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-05-07 12:02:18 +0200

    mutant stack iterators implemenation

--- Cpp08/ex02/MutantStack.hpp
+++ Cpp08/ex02/MutantStack.hpp
@@ -22,6 +22,29 @@ class MutantStack: public std::stack<T>
 			std::stack<T>::operator=(rhs);
 			return (*this);
 		};
+
+		// Iterator typedefs - these expose the underlying container's iterators
+		// std::stack has a protected member 'c' which is the container adapter
+		typedef typename std::stack<T>::container_type::iterator iterator;               // Regular forward iterator
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;   // Const forward iterator
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator; // Regular reverse iterator
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator; // Const reverse iterator
+
+		// Regular iterator methods - for mutable access to elements
+		iterator begin() { return this->c.begin(); }  // Returns iterator to first element
+		iterator end() { return this->c.end(); }      // Returns iterator to past-the-end element
+		
+		// Const iterator methods - for const access to elements
+		const_iterator begin() const { return this->c.begin(); }  // Returns const_iterator to first element
+		const_iterator end() const { return this->c.end(); }      // Returns const_iterator to past-the-end element
+		
+		// Reverse iterator methods - for iterating in reverse order
+		reverse_iterator rbegin() { return this->c.rbegin(); }    // Returns reverse_iterator to last element
+		reverse_iterator rend() { return this->c.rend(); }        // Returns reverse_iterator to before-the-first element
+		
+		// Const reverse iterator methods - for const reverse iteration
+		const_reverse_iterator rbegin() const { return this->c.rbegin(); }  // Returns const_reverse_iterator to last element
+		const_reverse_iterator rend() const { return this->c.rend(); }      // Returns const_reverse_iterator to before-the-first element
 };
 
 #endif
--- Cpp08/ex02/main.cpp
+++ Cpp08/ex02/main.cpp
@@ -14,8 +14,8 @@ int main()
 	mstack.push(737);
 	//[...]
 	mstack.push(0);
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
 	while (it != ite)
