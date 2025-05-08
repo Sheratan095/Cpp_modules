6fb3a4a4ec8a9c76f27cbe2c428bc72a29dd1985
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2025-05-08 17:05:19 +0200

    setting up cpp09/ex02

---
+++ Cpp09/ex01/RPN.cpp
@@ -0,0 +1,18 @@
+#include "RPN.hpp"
+
+Rpn::Rpn()
+{}
+
+Rpn::Rpn(const Rpn &source)
+{}
+
+Rpn::~Rpn()
+{}
+
+Rpn &Rpn::operator=(const Rpn &rhs)
+{
+	if (this == &rhs)
+		return (*this);
+
+	return (*this);
+}
---
+++ Cpp09/ex01/RPN.hpp
@@ -0,0 +1,14 @@
+#ifndef RNP_HPP
+#define RNP_HPP
+
+class Rpn
+{
+	public:
+		Rpn();
+		Rpn(const Rpn &src);
+		~Rpn();
+		Rpn &operator=(const Rpn &rhs);
+
+};
+
+#endif
