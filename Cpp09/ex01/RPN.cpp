38f9cce9b2b96eab1cb5fa6ff573813e39de8d0c
Marco Ceccarelli <marco.ceccarelli.code@gmail.com>
2025-05-08 17:18:31 +0200

    working on

--- Cpp09/ex01/RPN.cpp
+++ Cpp09/ex01/RPN.cpp
@@ -9,6 +9,11 @@ Rpn::Rpn(const Rpn &source)
 Rpn::~Rpn()
 {}
 
+float	Rpn::parseInput(const std::string& input) const
+{
+
+}
+
 Rpn &Rpn::operator=(const Rpn &rhs)
 {
 	if (this == &rhs)
--- Cpp09/ex01/RPN.hpp
+++ Cpp09/ex01/RPN.hpp
@@ -1,12 +1,18 @@
 #ifndef RNP_HPP
 #define RNP_HPP
 
+#include <iostream>
+#include <string>
+
 class Rpn
 {
 	public:
 		Rpn();
 		Rpn(const Rpn &src);
 		~Rpn();
+
+		float	parseInput(const std::string& input) const;
+
 		Rpn &operator=(const Rpn &rhs);
 
 };
